d557112465e7f7824b9df4d04a6b7fb00e566ab5
Adam Christopher B Widjaja <adam.keristoper@gmail.com>
2017-06-08 15:10:25 +0900

    Creating post test succeed!

--- spec/features/creating_post_spec.rb
+++ spec/features/creating_post_spec.rb
@@ -1,10 +1,28 @@
 require 'rails_helper'
 
+RSpec.configure do |config|
+  #database cleaner gem setup
+  config.use_transactional_fixtures = false
+
+  config.before(:suite) do
+  DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+  DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+  DatabaseCleaner.clean
+  end
+end
+
 RSpec.feature "Adam can create new posts" do
   let!(:category) { FactoryGirl.create(:category, category_name: "Photo Walk") }
 
-  scenario "with valid attributes" do
+  scenario "with valid attributes", js: true do
     visit new_post_path
+    expect(page).to have_content "Photo Walk"
     fill_in "post_title", with: "Crossing the Rainbow Bridge to Odaiba"
     click_button "new-block"
 
@@ -14,6 +32,10 @@ RSpec.feature "Adam can create new posts" do
     choose "Photo Walk"
 
     click_button "Create Post!"
-    expect(page.current_url).to eq new_post_url
+    sleep 1
+    expect(page.current_path).to eq posts_path
+    expect(page).to have_content "Crossing the Rainbow Bridge to Odaiba"
+    expect(page).to have_content "Photo Walk"
+    expect(page).to have_content "2016-04-10"
   end
 end
