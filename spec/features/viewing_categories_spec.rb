1b76bc3cb1f714014f91901c19fa0977069f97e8
Adam Christopher B Widjaja <adam.keristoper@gmail.com>
2017-07-11 18:17:03 +0900

    Unpublished posts are not shown in categories page

--- spec/features/viewing_categories_spec.rb
+++ spec/features/viewing_categories_spec.rb
@@ -3,10 +3,16 @@ require 'rails_helper'
 RSpec.feature "Anyone can view posts" do
   let!(:category) { FactoryGirl.create :category }
   let!(:post) { FactoryGirl.create :post, category: category }
+  let!(:post2) { FactoryGirl.create :post,
+    category: category,
+    status: "unpublished",
+    title: "Unpublished post"
+  }
 
   scenario "successfully" do
     visit "/blog/categories/tokyo-cycling-diary"
     expect(page).to have_content "Tokyo Cycling Diary"
     expect(page).to have_content "A test post"
+    expect(page).to_not have_content "Unpublished post"
   end
 end
